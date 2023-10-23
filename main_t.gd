99899f4cc7875487a038cf97df0c7d533aa4bff1
kaveh ghavany <ghavamy@gmail.com>
2023-10-23 11:13:37 -0700

    object pooling main script added

--- main_t.gd
+++ main_t.gd
@@ -1,16 +1,37 @@
 extends Node2D
 
+const Pool = preload("res://pool.gd")
+const bullet = preload("res://laser.tscn")
+
+const BULLET_POOL_SIZE = 20
+const BULLET_POOL_PREFIX = "bullet"
+
 var laser_scene = preload("res://laser.tscn")
 var grenade_scene = preload("res://grenade.tscn")
 var enemy_scene = preload("res://enemy_ai.tscn")
 
+var bullets
+var player
+var pool
 
+func _ready():
+	bullets = get_node("projectles")
+	player = get_node("player")
+	pool = Pool.new(BULLET_POOL_SIZE, BULLET_POOL_PREFIX, bullet)
+	
+	pool.add_to_node(bullets)
+	pool.connect("killed", self, "_on_pool_killed")
+	set_process_input(true)
+	
 func _on_player_laser():
-	var laser = laser_scene.instantiate() as Area2D
+#	var laser = laser_scene.instantiate() as Area2D
+	var laser = pool.get_first_dead()
 	laser.position = $player/Gun1.pos
 	laser.direction = (get_global_mouse_position() - $player.global_position).normalized()
-	add_child(laser)
+#	add_child(laser)
 
+func _on_pool_killed(target):
+	target.hide()
 
 
 func _on_player_grenade():
---
+++ pool.gd
@@ -0,0 +1,85 @@
+signal killed(target)
+
+var prefix #prefix of the object spwaned
+var size #for pool size
+var scene #preloaded scene
+
+var alive = {} #for alive objects in pool
+var dead = [] #list of dead objects
+
+func _init(size_,prefix_,scene_):
+	size = size_
+	prefix = prefix_
+	scene = scene_
+	init()
+	
+func init():
+	
+	if scene == null:
+		return
+	
+	for i in range(size):
+		var s = scene.instantiate()
+		s.set_name(prefix + "_" + str(i))
+		s.connect("killed", self , "on_killed")
+		dead.push_back(s)
+
+func get_first_dead():
+	var ds = dead.size()
+	if ds > 0:
+		var o = dead[ds - 1]
+		if !o.dead: return null
+
+		var n = o.get_name()
+		alive[n] = o
+		dead.pop_back()
+		o.dead = false
+		o.set_pause_mode(0)
+		return o
+
+	return null
+	
+func get_first_alive():
+	if alive.size() > 0:
+		return alive.values()[0]
+
+	return null
+	
+func kill_all():
+	for i in alive.values():
+		i.kill()
+
+func add_to_node(node):
+	for i in alive.values():
+		node.add_child(i)
+
+	for i in dead:
+		node.add_child(i)
+
+func show():
+	for i in alive.values():
+		i.show()
+
+	for i in dead:
+		i.show()
+
+func hide():
+	for i in alive.values():
+		i.hide()
+
+	for i in dead:
+		i.hide()
+
+func _on_killed(target):
+	# Get the name of the target object that was killed
+	var name = target.get_name()
+
+	# Remove the killed object from the alive pool
+	alive.erase(name)
+
+	# Add the killed object to the dead pool, now available for use
+	dead.push_back(target)
+
+	target.set_pause_mode(1)
+
+	emit_signal("killed", target)
